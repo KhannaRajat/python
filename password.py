1423afd299a066c208e4a93cc8fa9bca1e9b3769
KhannaRajat <43250245+KhannaRajat@users.noreply.github.com>
2020-07-11 20:12:04 +0530

    Delete password.py

--- password.py
+++
@@ -1,9 +0,0 @@
-from random_password_generator import password_generator as pg
-
-print('u: upper, l: lower, d: digits, s: Symbols, ul: upper-lower, lds: lower-digits-symbols')
-character = input('Enter the password type: ')
-length = input('Length of password: ')
-
-password = pg.PasswordGenerator(character, length)
-password.set_charset()
-password.generate_password()
